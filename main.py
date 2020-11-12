687308230e60cfe6deeb1b85a199aeb389a00a75
MrHilicks <72390856+MrHilicks@users.noreply.github.com>
2020-11-12 16:08:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from imageai.Detection import ObjectDetection
+import os
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath("yolo-tiny.h5")
+detector.loadModel(detection_speed='fastest')
+custom = detector.CustomObjects(person=True)
+
+detections = detector.detectCustomObjectsFromImage(custom_objects=custom,
+                                                   input_image=os.path.join(execution_path, "image.jpg"),
+                                                   output_image_path=os.path.join(execution_path, "image_new.jpg"),
+                                                   minimum_percentage_probability=30)
+
+persons = len(detections)
+print('Количество людей в очереди: ' + str(persons))
